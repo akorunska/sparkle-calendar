2325498126a10e0557ed91910b205d40b08f3836
August3 <9rails@gmail.com>
2017-12-19 05:09:23 +0200

    2 passwords to enter

--- src/public/javascripts/validate_signup.js
+++ src/public/javascripts/validate_signup.js
@@ -1,6 +1,9 @@
 function validate() {
-    let value = document.getElementById('pass_input').value;
-    if (value.length < 6) {
+    let pass1 = document.getElementById('pass_input').value;
+    let pass2 = document.getElementById('pass_input2').value;
+    if (pass1.length < 6) {
+        return false;
+    } else if (pass1 !== pass2) {
         return false;
     }
 }
@@ -11,6 +14,18 @@ function validate_pass(pass) {
     if (value.length < 6) {
         error.style.visibility = 'visible';
         error.innerText = 'Password must be at least 6 symbols long'
+    } else {
+        error.style.visibility = 'hidden';
+    }
+}
+
+function validate_pass_match(password) {
+    let pass1 = document.getElementById('pass_input').value;
+    let pass2 = document.getElementById('pass_input2').value;
+    let error = document.getElementById('pass_error2');
+    if (pass1 !== pass2) {
+        error.style.visibility = 'visible';
+        error.innerText = 'Passwords should match'
     } else {
         error.style.visibility = 'hidden';
     }
