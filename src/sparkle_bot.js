75708822a7c8912a85993614b3037ff4f6508adb
August3 <9rails@gmail.com>
2017-12-21 16:38:19 +0200

    impoved /today

--- src/sparkle_bot.js
+++ src/sparkle_bot.js
@@ -6,9 +6,8 @@ let users = require('./modules/users.js');
 let events = require('./modules/events.js');
 
 let token = '471423777:AAFuKixbmlNxadC5qVnxh0TYXkDvuRnV6yU';
+
 let bot = new telegram_bot(token, {polling: true});
-
-
 
 bot.onText(/\/start/, function (msg) {
     let from_id = msg.from.id;
@@ -16,9 +15,8 @@ bot.onText(/\/start/, function (msg) {
     let instr =
         "Hello, " + msg.from.username +", I`m Sparkle Bot.\n" +
         "I can help you with notifications for your events on Sparkle Calendar\n" +
-        "/start -- see this message again\n" +
-        "/echo -- repeat your message\n" +
-        "/today -- see events for today\n" +
+        "/start — see this message again\n" +
+        "/today — see events for today\n" +
         "\n" +
         "If you`ve got any issues, questions or propositions, contact @augustusTertius";
 
@@ -50,82 +48,107 @@ bot.onText(/\/start/, function (msg) {
         });
 });
 
-bot.onText(/echo (.+)/, function (msg, match) {
-    let from_id = msg.from.id;
-    let resp = match[1];
-    bot.sendMessage(from_id, resp);
-});
-
-bot.onText(/\/today/, function (msg, match) {
+bot.onText(/\/today/, async function (msg) {
     let resp = "";
     let from_id = msg.from.id;
+    let user;
 
-    telegram_users.getUserByTelegramUsername(msg.from.username)
-        .then(user => {
-            if (user) {
-                let date = moment();
-                let search_date = (date.format()).substring(0, (date.format()).indexOf('T'));
-                console.log(search_date);
-                events.getByDate(user.user_id, search_date)
-                    .then((events => {
-                        console.log(events.length);
-                        if (events.length > 0) {
-                            bot.sendMessage(from_id, JSON.stringify(events));
-                        } else {
-                            resp = "You`ve nothing planned on this date";
-                            bot.sendMessage(from_id, resp);
-                        }
-                    }))
-                    .catch((err) => {
-                        resp = "An error occurred, sorry.";
-                        bot.sendMessage(from_id, resp);
-                    })
-            } else {
-                resp = "Seems you`re not subscribed in the bot. Try running /start.";
-                bot.sendMessage(from_id, resp);
-            }
-        })
-        .catch((err) => {
-            console.log(err);
-            resp = "An error occurred, sorry.";
-            bot.sendMessage(from_id, resp);
-        });
+    try {
+        user = await telegram_users.getUserByTelegramUsername(msg.from.username);
+        console.log(user);
+    } catch(e) {
+        console.log(e);
+        resp = "An error occurred, sorry.";
+        bot.sendMessage(from_id, resp);
+    }
+
+    if (user) {
+        let date = moment();
+        let search_date = (date.format()).substring(0, (date.format()).indexOf('T'));
+        eventsForDate(user, from_id, search_date);
+    } else {
+        resp = "Seems you`re not subscribed in the bot. Try running /start.";
+        bot.sendMessage(from_id, resp);
+    }
 });
 
-bot.onText(/\/love/, function onLoveText(msg) {
-    const opts = {
-        reply_to_message_id: msg.message_id,
-        reply_markup: JSON.stringify({
-            'one_time_keyboard': true,
-            keyboard: [
-                ['Yes, you are the bot of my life ❤'],
-                ['No, sorry there is another one...']
-            ]
-        })
-    };
+async function eventsForDate(user, from_id, search_date) {
+    let event_list;
 
-    bot.sendMessage(msg.from.id, 'Do you love me?', opts)
-        .then(sent => {
-            bot.onReplyToMessage(msg.from.id, sent.message_id, (response) => {
-                if (response.text === "Yes, you are the bot of my life ❤") {
-                    bot.sendMessage(msg.from.id, 'It is forever :3')
-                }
-            });
-            bot.onText("Yes, you are the bot of my life ❤", () => {
-                bot.sendMessage(msg.from.id, 'It is forever :3')
-            });
-        });
+    try {
+       event_list = await events.getByDate(user.user_id, search_date);
+    } catch(e) {
+        console.log(e);
+        resp = "An error occurred, sorry.";
+        bot.sendMessage(from_id, resp);
+    }
 
-    // {
-    //     "reply_markup": {
-    //     "remove_keyboard": true
-    // }
-    // }
+    if (event_list.length > 0) {
+        let buttons = [];
+        for (let ev of event_list) {
+            let info = {
+                action: 'show',
+                id: ev.id,
+            };
+            let info_data = JSON.stringify(info);
+            let button = [
+                {text: ev.name, callback_data: JSON.stringify({action: 'show', id: ev.id}) },
+                {text: '✏', callback_data: JSON.stringify({action: 'more', id: ev.id}) }
+            ];
+            buttons.push(button);
+        }
+
+        let options = {
+            reply_markup: JSON.stringify({
+                inline_keyboard: buttons,
+                parse_mode: 'Markdown'
+            })
+        };
+
+        bot.sendMessage(from_id, 'Here are your events for ' + search_date, options);
+    } else {
+        resp = "You`ve got nothing planned on " + search_date;
+        bot.sendMessage(from_id, resp);
+    }
+}
+
+bot.on('callback_query', async function (msg) {
+    let input = JSON.parse(msg.data);
+
+    if (input.action === 'show') {
+        let event = await events.getById(input.id);
+        let options = {
+            parse_mode : "Markdown"
+        };
+
+        let resp = `*${event.name}*\n\n` +
+        `**Time**:  ${event.start_time} - ${event.end_time}; ${moment(event.date).format("dddd, MMMM Do YYYY")}\n` +
+        `**Place**: ${event.place}`;
+
+        bot.sendMessage(msg.from.id, resp, options);
+        bot.answerCallbackQuery(msg.id);
+    } else if (input.action === 'more') {
+        let event = await events.getById(input.id);
+
+        let buttons = [
+            [{text: 'edit event', callback_data: JSON.stringify({action: 'edit', id: event.id}) },
+            {text: 'delete event', callback_data: JSON.stringify({action: 'delete', id: event.id}) }]
+        ];
+        let options = {
+            parse_mode: 'Markdown',
+            reply_markup: JSON.stringify({
+                inline_keyboard: buttons,
+            })
+        };
+        let resp = `What would you like to do with *${event.name}*?`;
+        bot.sendMessage(msg.from.id, resp, options);
+        bot.answerCallbackQuery(msg.id);
+    }
 });
 
-// bot.on('message', function (msg) {
-//     let chat_id = msg.chat.id;
-//     // Фотография может быть: путь к файлу, поток(stream) или параметр file_id
-//     let photo = 'public/images/default.jpg';
-//     bot.sendPhoto(chat_id, photo, {caption: 'You wanted a pic? Here you go.'});
-// });
+bot.onText(/\/echo (.+)/, function (msg, match) {
+    let from_id = msg.from.id;
+    let resp = match[1];
+    console.log(match[1]);
+    bot.sendMessage(from_id, resp);
+});
